49796e154f9a6a708fa37e04ec75af66c8eb0609
Nadiia Krzesinska <nb1985@ukr.net>
2025-12-31 13:31:55 +0000

    feat: add custom validation messages

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,13 @@
+import { IsBoolean, IsNotEmpty, IsString, Length } from "class-validator";
+
 export class UpdateTaskDto {
+  @IsString({ message: 'Title must be a string' })
+  @IsNotEmpty()
+  @Length(
+    2, 10, { message: 'Min length: 2 characters, max length: 10 characters' }
+  )
   title: string;
+
+  @IsBoolean({ message: 'Value must be a boolean'})
   isCompleted: boolean;
 }
