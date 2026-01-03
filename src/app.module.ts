630c5f5ff9db971b10c8b96debfec0cc4f5a2552
Nadiia Krzesinska <nb1985@ukr.net>
2026-01-03 15:43:26 +0000

    refactor(config): move database config object to separate folder for consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeormConfig } from './config/typeorm.config';
 
 
 @Module({
@@ -14,16 +15,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: +configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        autoLoadEntities: true,
-        synchronize: true
-      }),      
+      imports: [ConfigModule],
+      useFactory: getTypeormConfig,      
     }),
     TaskModule, 
     MovieModule
