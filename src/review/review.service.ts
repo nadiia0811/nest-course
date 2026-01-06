411e95b48daddc09162f9cccd632cc61f966ab04
Nadiia Krzesinska <nb1985@ukr.net>
2026-01-06 14:34:02 +0000

    feat(review): include movie in review creation response

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -8,23 +8,23 @@ import { MovieService } from 'src/movie/movie.service';
 
 @Injectable()
 export class ReviewService {
-    constructor(
-        @InjectRepository(ReviewEntity)
-        private readonly reviewRepository: Repository<ReviewEntity>,
-        private readonly movieService: MovieService
-    ) {}
+  constructor(
+    @InjectRepository(ReviewEntity)
+    private readonly reviewRepository: Repository<ReviewEntity>,
+    private readonly movieService: MovieService
+  ) { }
 
-    async createReview(dto: CreateReviewDto): Promise<ReviewEntity> {
-      const movie = await this.movieService.getMovieById(dto.movieId);
+  async createReview(dto: CreateReviewDto): Promise<ReviewEntity> {
+    const movie = await this.movieService.getMovieById(dto.movieId);
 
-      if (!movie) {
-        throw new NotFoundException('Movie not found');
-      }
-
-      const { text, rating, movieId } = dto;
-      const review = this.reviewRepository.create({ text, rating, movieId });
-    
-      await this.reviewRepository.save(review);
-      return review;
+    if (!movie) {
+      throw new NotFoundException('Movie not found');
     }
+
+    const { text, rating, movieId } = dto;
+    const review = this.reviewRepository.create({ text, rating, movieId, movie });
+
+    await this.reviewRepository.save(review);
+    return review;
+  }
 }
