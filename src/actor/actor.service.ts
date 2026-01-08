5b44705388589d6f08cba455a677dc5ea9caacfc
Nadiia Krzesinska <nb1985@ukr.net>
2026-01-08 14:50:34 +0000

    feat(actor): add actor DTO and creation logic

--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ActorEntity } from './entities/actor.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateActorDto } from './dto/create-actor.dto';
 
 @Injectable()
-export class ActorService {}
+export class ActorService {
+    constructor(
+      @InjectRepository(ActorEntity)
+      private readonly actorRepository: Repository<ActorEntity>,
+    ) {}
+
+    async createActor(dto: CreateActorDto): Promise<ActorEntity> {
+        const actor = this.actorRepository.create(dto);
+
+        return await this.actorRepository.save(actor);
+    }
+}
